aa74211810f6c4e80915fd9fde89fa90a8150080
vviital <vviital@gmail.com>
2017-07-23 14:52:37 +0300

    fifth lesson

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+function* create3to4Counter() {
+	yield 3;
+	return 4;
+}
+
 function* createCounter() {
 	yield 1;
 	yield 2;
-	yield 3;
-	yield 4;
+	const four = yield* create3to4Counter();
+	console.log(four);
 	yield 5;
 }
 
 const counter = createCounter();
 
-for (let i of counter) {
+for(let i of counter) {
 	console.log(i);
 }
