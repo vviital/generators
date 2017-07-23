f7222e7c74aa36c8e3d74ed5f784e67286595d66
vviital <vviital@gmail.com>
2017-07-23 13:52:49 +0300

    first-lesson

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-/**
- * Created by vviital on 7/23/17.
- */
+function* createLogger() {
+	console.log('start');
+
+	yield;
+
+	console.log('Second block');
+
+
+	yield;
+
+	console.log('Third block');
+
+	yield;
+
+	console.log('end');
+}
+
+const logger = createLogger();
+
+console.log(logger.next());
+console.log(logger.next());
+console.log(logger.next());
+console.log(logger.next());
+console.log(logger.next());
