94992fe940cafe08f33ca4990bddd774360b6b89
vviital <vviital@gmail.com>
2017-07-23 15:47:20 +0300

    sixth lesson (part 3)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 const fetch = require('node-fetch');
+const co = require('co');
 
 function* createQuoteFetcher() {
 	const response = yield fetch(url);
@@ -7,20 +8,7 @@ function* createQuoteFetcher() {
 	return `${quote.quoteText} - ${quote.quoteAuthor}`;
 }
 
-function coroutine(gen) {
-	const generator = gen();
-
-	function handler(result) {
-		if (result.done) return Promise.resolve(result.value);
-
-		return Promise.resolve(result.value)
-			.then(res => handler(generator.next(res)));
-	}
-
-	return handler(generator.next());
-}
-
-const quoteFetcher = coroutine(createQuoteFetcher);
+const quoteFetcher = co(createQuoteFetcher);
 
 quoteFetcher
 	.then(quote => console.log(quote))
